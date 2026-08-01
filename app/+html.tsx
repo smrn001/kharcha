f2d2439480a69186462124c0dfa223b745a40869
smrn001 <samir.niroula001@gmail.com>
2026-08-01 10:41:10 +0545

    feat: add web favicon set and PWA manifest
    - Move favicon_io set to public/ (favicon.ico, 16/32px, apple-touch-icon, android-chrome, site.webmanifest)
    - Link favicons in web head (+html.tsx) and brand site.webmanifest
    - Point web.favicon to public/favicon-32x32.png
    - Remove unused default assets/images/favicon.png

--- app/+html.tsx
+++ app/+html.tsx
@@ -19,7 +19,12 @@ export default function Root({ children }: PropsWithChildren) {
         */}
         <ScrollViewStyleReset />
 
-        {/* Add any additional <head> elements that you want globally available on web... */}
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="theme-color" content="#4f46e5" />
       </head>
       <body>{children}</body>
     </html>
